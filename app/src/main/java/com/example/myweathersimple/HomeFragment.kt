e7d3bfff94666148184880bb3bd22169e3004c2b
raulzitoe <raulguil@gmail.com>
2022-05-17 11:47:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myweathersimple/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.example.myweathersimple
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import com.example.myweathersimple.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
---
+++ app/src/main/java/com/example/myweathersimple/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.myweathersimple
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
